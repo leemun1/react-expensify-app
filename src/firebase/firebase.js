4748507fed53a9588d6cf18ba35420a63bffab35
Mike <munkyu0813@gmail.com>
2018-05-17 11:06:30 -0400

    connect firebase with redux

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,19 +13,86 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
-  name: "Mike Lee",
-  age: 26,
-  isSingle: true,
-  location: {
-    city: 'Toronto',
-    country: 'Canada'
-  }
-});
+export { firebase, database as default };
 
-database.ref('location/city').set('Montreal');
 
-database.ref('attributes').set({
-  height: 173,
-  weight: 67
-});
+// database.ref('expenses')
+//   .once('value')
+//   .then((snapshot) => {
+//     const expenses = [];
+
+//     snapshot.forEach((childSnapshot) => {
+//       expenses.push({
+//         id: childSnapshot.key,
+//         ...childSnapshot.val()
+//       });
+//     });
+
+//     console.log(expenses);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// database.ref('expenses')
+//   .on('value', (snapshot) => {
+//     const expenses = [];
+
+//     snapshot.forEach((childSnapshot) => {
+//       expenses.push({
+//         id: childSnapshot.key,
+//         ...childSnapshot.val()
+//       });
+//     });
+//     console.log(expenses);
+//   }, (err) => {
+//     console.log(err);
+//   });
+
+// database.ref('expenses').push({
+//   description: 'Gas Bill',
+//   note: '',
+//   amount: 17400,
+//   createdAt: 20
+// });
+
+// database.on('value', (snapshot) => {
+//   const val = snapshot.val();
+//   console.log(val);
+// }, (err) => {
+//   console.log(err);
+// });
+
+// database.ref('name')
+//   .once('value')
+//   .then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// database.ref().set({
+//     name: "Mike Lee",
+//     age: 26,
+//     isSingle: true,
+//     location: {
+//       city: 'Toronto',
+//       country: 'Canada'
+//     }
+//   })
+//   .then(() => {
+//     console.log('Data saved!');
+//   })
+//   .catch((err) => {
+//     console.log('This failed.', err);
+//   });
+
+// database.ref('isSingle').remove()
+//   .then(() => {
+//     console.log('isSingle was removed');
+//   })
+//   .catch((err) => {
+//     console.log('Did not remove data', err);
+//   });
